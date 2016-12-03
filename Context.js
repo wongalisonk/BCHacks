bdaf61e16fe27b29433b97c7a82fc457a6aa93ac
Alison Wong <wongalisonk@gmail.com>
2016-12-03 15:21:25 -0800

    Got button working

--- Context.js
+++ Context.js
@@ -4,15 +4,19 @@ var details = [];
 var dueDates = []; 
 var times = []; 
 
+document.addEventListener('DOMContentLoaded', function() {
+	var button = document.getElementById('button');
+	button.addEventListener('click', function() {
+		submitForm();
+	});
+});
+
 function submitForm() {
+	alert();
 	names.push(document.getElementById("TaskName"));
 	details.push(document.getElementById("Detail"));
 	dueDates.push(document.getElementById("Date"));
 	times.push(document.getElementById("TimeNeeded"));
 
 	document.getElementById("insert").innerHTML = names.toString;
-};
-
-function notify(){
-	alert(); 
 };

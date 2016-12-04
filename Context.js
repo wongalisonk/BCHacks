44f43b7cdba90cddb125006f92ac9abcc93c0b3c
ArdalanAhanchi <Ardalan@ahanchi.net>
2016-12-03 18:33:36 -0800

    Add files via upload

--- Context.js
+++ Context.js
@@ -6,7 +6,6 @@ var Time ;
 
 var Tasks = [];
 var current ; 
-var count = 0;
 
 function Task(TaskName,TaskDetails,TaskDueDate,TaskTime){
 	this.Name = TaskName ; 
@@ -25,33 +24,59 @@ document.addEventListener('DOMContentLoaded', function() {
 		current = Tasks.length -1 ; 
 		Refresh(); 
 		
-		count++;
-		chrome.notifications.create(
-		'Basic Notification', {
-			type: 'basic',
-			iconUrl: 'icon.png',
-			title: 'Boop',
-			message: "You now have " + count + " task(s)"
-		}
-	);
+		alert("New Task Added"); 
     });
 });
 
 document.addEventListener('DOMContentLoaded', function() { 
-    var link = document.getElementById('DeleteTasks');
+    var link = document.getElementById('deleteTask');
+	 
     // onClick's logic below:
-    link.addEventListener('click', function() { 
-		Tasks.splice(index, current); 
-		alert(); 
+    link.addEventListener('click', function() {		
+		Tasks.splice(current, 1) 
 		current = Tasks.length - 1;
-		if(current < 0){
-			current = 0; 
-		}
+		Refresh(); 
 		alert("Good Job!"); 
     });
 });
 
+document.addEventListener('DOMContentLoaded', function() { 
+    var link = document.getElementById('next');
+	 
+    // onClick's logic below:
+    link.addEventListener('click', function() {
+		if ( current != Tasks.length -1 ){
+			current = current + 1
+		}
+		Refresh(); 
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() { 
+    var link = document.getElementById('previous');
+	 
+    // onClick's logic below:
+    link.addEventListener('click', function() {
+		if ( current != 0 ){
+			current = current - 1
+		}
+		Refresh(); 
+    });
+});
+
 function Refresh(){
+		if(current < 0){
+			current = 0; 
+		} ;
+		
+		if(Tasks.length == 0){
+			document.querySelector('.Nameresults').innerHTML = "";
+			document.querySelector('.Detailresults').innerHTML = "";
+			document.querySelector('.Dateresults').innerHTML = "";
+			document.querySelector('.Timeresults').innerHTML = "";
+		};
+		
+		
 		document.querySelector('.Nameresults').innerHTML = Tasks[current].Name;
 		document.querySelector('.Detailresults').innerHTML = Tasks[current].Details;
 		document.querySelector('.Dateresults').innerHTML = Tasks[current].DueDate;
